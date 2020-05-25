d47dc1d10ce47250fab54907f694ef2174930745
moni-roy <roy.moni.ju.cse@gmail.com>
2020-05-25 18:36:48 +0600

    [Section-3] Props and passing children components.

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetails from "./CommentDetails";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetails
-        author="Moni"
-        timeAgo="Today at 5:58PM"
-        content="Nice to start!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetails
-        author="Roy"
-        timeAgo="Today at 5:38PM"
-        content="Good to practice!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetails
-        author="MRoy"
-        timeAgo="Yesterday at 5:30PM"
-        content="Nice blog post!"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="Moni"
+          timeAgo="Today at 5:58PM"
+          content="Nice to start!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="Roy"
+          timeAgo="Today at 5:38PM"
+          content="Good to practice!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="MRoy"
+          timeAgo="Yesterday at 5:30PM"
+          content="Nice blog post!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
