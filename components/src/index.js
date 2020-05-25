9307aace3e8be3ecbc1b43b1f4d38f75b956bc17
moni-roy <roy.moni.ju.cse@gmail.com>
2020-05-25 18:06:46 +0600

    [Section-3] Usage of Props and passing data

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 import CommentDetails from "./CommentDetails";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetails />
-      <CommentDetails />
-      <CommentDetails />
+      <CommentDetails
+        author="Moni"
+        timeAgo="Today at 5:58PM"
+        content="Nice to start!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetails
+        author="Roy"
+        timeAgo="Today at 5:38PM"
+        content="Good to practice!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetails
+        author="MRoy"
+        timeAgo="Yesterday at 5:30PM"
+        content="Nice blog post!"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
