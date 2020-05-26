32e5efbedeef739f80f5e9a9ad1cf04f8420dee5
moni-roy <roy.moni.ju.cse@gmail.com>
2020-05-26 20:46:29 +0600

    [Section-5] State in React Components
    - Initialing State Through Constructors.
    - Updating State Properties.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,12 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    // initialize the state
+    // THIS IS ONLY THE tiME we do direct assignment
+    this.state = { lat: null };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        console.log(position);
+        // set state by calling setstate
+        this.setState({ lat: position.coords.latitude });
+      },
       (err) => console.log(err)
     );
-    return <div>Latitude: </div>;
+  }
+
+  // React says we have to define render!!
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
